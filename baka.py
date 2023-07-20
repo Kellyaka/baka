663c25613c4b6c6ca454f16e75f8c3ced5284cb8
Kelly <72246241+Kellyaka@users.noreply.github.com>
2023-07-20 13:54:03 +0530

    Update baka.py

--- baka.py
+++ baka.py
@@ -1,4 +1,31 @@
 class Baka:
+
+  def subred():
+    
+    r_list = []
+    return r_list
+    
+  def reddit():
+    
+    r_client_secret = ""
+    r_client_id = ""
+    r_client_username = ""
+    r_client_pass = ""
+    r_client_agent = ""
+    return [r_client_secret, r_client_id, r_client_username, r_client_pass, r_cilent_agent]
+    
+  def color():
+    c = 0x8F00FF
+    return c
+  
+  def tag():
+    tags = []
+    return tags
+  def mongodb():
+    url = ""
+    table = ""
+    c - ""
+    return [url, table, c]
   def token():
     t = ""
     return t
